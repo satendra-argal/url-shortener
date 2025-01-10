bff279e0f132643fd9eeff557251d79fae430196
Satendra <satendra.argal123@gmail.com>
2025-01-10 15:41:52 +0530

    url shorten funtion is working fine

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
 )
@@ -44,11 +46,83 @@ func generateShortURL() string {
 const baseURL = "http://localhost:8080/"
 const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+//DomainExtraction from the Given URL
+
+func DomainExtract(url string) string {
+	if strings.HasPrefix(url, "http://") {
+		url = strings.TrimPrefix(url, "http://")
+
+	} else if strings.HasPrefix(url, "https://") {
+		url = strings.TrimPrefix(url, "http://")
+	}
+	domain := strings.Split(url, "/")[0]
+	return domain
+}
+
+// shortenURLHandler handles the POST request to shorten a URL
+func shortenURLHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+	var requestData struct {
+		URL string `json:"url"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&requestData); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+	}
+
+	shortener.mutex.Lock()
+
+	defer shortener.mutex.Unlock()
+
+	if shortURL, exists := shortener.reverseMap[requestData.URL]; exists {
+		response := map[string]string{"short_url": baseURL + shortURL}
+		json.NewEncoder(w).Encode(response)
+		return
+	}
+	shortURL := generateShortURL()
+	for _, exists := shortener.urlMap[shortURL]; exists; {
+		shortURL = generateShortURL()
+	}
+
+	shortener.urlMap[shortURL] = requestData.URL
+
+	shortener.reverseMap[requestData.URL] = shortURL
+
+	domain := DomainExtract(requestData.URL)
+	shortener.domainCount[domain]++
+
+	response := map[string]string{"short_url": baseURL + shortURL}
+	json.NewEncoder(w).Encode(response)
+
+}
+
+// redirectHandler handles redirection requests for short URLs
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	shortURL := strings.TrimPrefix(r.URL.Path, "/")
+
+	shortener.mutex.Lock()
+	defer shortener.mutex.Unlock()
+
+	originalURL, exists := shortener.urlMap[shortURL]
+	if !exists {
+		http.Error(w, "Short URL not found", http.StatusNotFound)
+		return
+	}
+
+	http.Redirect(w, r, originalURL, http.StatusFound)
+}
+
 func main() {
 	fmt.Println("Url Shortener started........ ")
+
+	http.HandleFunc("/shorten", shortenURLHandler)
 
 	log.Println("server started at : 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
+
 }
