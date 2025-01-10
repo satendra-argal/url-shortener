492a626140f0ac70aeb2e9d9388294cb0155a018
Satendra <satendra.argal123@gmail.com>
2025-01-10 16:12:27 +0530

    topdomain count working fine

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"sort"
 	"strings"
 	"sync"
 	"time"
@@ -53,7 +54,7 @@ func DomainExtract(url string) string {
 		url = strings.TrimPrefix(url, "http://")
 
 	} else if strings.HasPrefix(url, "https://") {
-		url = strings.TrimPrefix(url, "http://")
+		url = strings.TrimPrefix(url, "https://")
 	}
 	domain := strings.Split(url, "/")[0]
 	return domain
@@ -73,6 +74,7 @@ func shortenURLHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 	}
 
+	// Lock the mutex for thread-safe operations
 	shortener.mutex.Lock()
 
 	defer shortener.mutex.Unlock()
@@ -103,6 +105,7 @@ func shortenURLHandler(w http.ResponseWriter, r *http.Request) {
 func redirectHandler(w http.ResponseWriter, r *http.Request) {
 	shortURL := strings.TrimPrefix(r.URL.Path, "/")
 
+	// Lock the mutex for thread-safe operations
 	shortener.mutex.Lock()
 	defer shortener.mutex.Unlock()
 
@@ -115,11 +118,60 @@ func redirectHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, originalURL, http.StatusFound)
 }
 
+// topDomains handles requests to retrieve domain Count
+func topDomains(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Lock the mutex for thread-safe operations
+	shortener.mutex.Lock()
+	defer shortener.mutex.Unlock()
+
+	// Map to hold the count of each domain
+	domainCounts := make(map[string]int)
+
+	// Iterate through the reverse map and extract domain from each original URL
+	for originalURL := range shortener.reverseMap {
+		domain := DomainExtract(originalURL)
+		domainCounts[domain]++
+	}
+
+	// Convert the domainCounts map to a slice of structs for sorting
+	type domainCount struct {
+		Domain string `json:"domain"`
+		Count  int    `json:"count"`
+	}
+
+	var counts []domainCount
+	for domain, count := range domainCounts {
+		counts = append(counts, domainCount{Domain: domain, Count: count})
+	}
+
+	// Sort the counts in descending order by count
+	sort.Slice(counts, func(i, j int) bool {
+		return counts[i].Count > counts[j].Count
+	})
+
+	// Get the top 3 domains
+	if len(counts) > 3 {
+		counts = counts[:3]
+	}
+
+	// Encode the result as JSON and send the response
+	json.NewEncoder(w).Encode(counts)
+
+}
+
+// main function up the HTTP server and routes
 func main() {
 	fmt.Println("Url Shortener started........ ")
 
+	// Define routes for URL shortening, metrics, and redirection
 	http.HandleFunc("/shorten", shortenURLHandler)
 	http.HandleFunc("/", redirectHandler)
+	http.HandleFunc("/top-domains", topDomains)
 
 	log.Println("server started at : 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
