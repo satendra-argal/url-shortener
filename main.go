802c79b38f82a080a68023758715287803df6ee8
Satendra <satendra.argal123@gmail.com>
2025-01-10 15:50:31 +0530

    redirect function  is working now

--- main.go
+++ main.go
@@ -119,6 +119,7 @@ func main() {
 	fmt.Println("Url Shortener started........ ")
 
 	http.HandleFunc("/shorten", shortenURLHandler)
+	http.HandleFunc("/", redirectHandler)
 
 	log.Println("server started at : 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
